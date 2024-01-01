cb7b931f9656f9a853ff10a5bbb76ba94affc0fa
WaveBleak <Mikkelcr3@gmail.com>
2024-01-01 13:44:51 +0100

    First Commit

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Loader;
+using System.Text;
+
+class Program
+{
+    private static int _port;
+    private static readonly IPAddress broadcastAddress = IPAddress.Parse("127.0.0.1"); //Localhost addressen
+    static async Task Main(string[] args)
+    {
+        await AskForPort(); //_port bliver sat
+        UdpClient client = new UdpClient(); //UDP Client laves
+        IPEndPoint endPoint = new IPEndPoint(broadcastAddress, _port); //Forbindelse til localhost med portet _port;
+
+        try
+        {
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Skriv en besked som skal sendes til serveren!");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(message); //Lav en besked om til bytes som kan sendes over netværket
+                int attempts = 0;
+                await client.SendAsync(data, data.Length, endPoint); //Send bytes over netværk
+            }
+        }
+        catch (Exception) //Noget gik galt
+        {
+            Console.WriteLine("Kan ikke finde en server på port: " + _port);
+            Console.ReadKey();
+            return;
+        }finally
+        {
+            client.Close();
+        }
+    }
+    
+    private static Task<bool> AskForPort()
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Skriv hvilket port du vil skrive til");
+                    String input = Console.ReadLine();
+                    if (input == null) throw new Exception();
+
+                    _port = int.Parse(input);
+                    tcs.SetResult(true);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Noget gik galt! Prøv igen.");
+                    Console.ReadKey();
+                }
+            }
+        });
+        return tcs.Task;
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+class Program
+{
+    private static int _port;
+    static async Task Main(string[] args)
+    {
+        await AskForPort(); //sæt _port
+        Console.Clear();
+        UdpClient listener = new UdpClient(_port);
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, _port); //Accepter connections hvorsomhelst fra så længe det er det rigtige port
+
+        try
+        {
+            Console.WriteLine("Connection established!");
+            while (true)
+            {
+                byte[] bytes = listener.Receive(ref groupEP); //Læs bytes fra connections
+                Console.WriteLine(Encoding.ASCII.GetString(bytes)); //Converter bytes til en besked
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            listener.Close();
+        }
+
+    }
+
+
+    private static Task<bool> AskForPort()
+    {
+        var tcs = new TaskCompletionSource<bool>();
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Skriv hvilket port du vil lytte på");
+                    String input = Console.ReadLine();
+                    if (input == null) throw new Exception();
+
+                    _port = int.Parse(input);
+                    tcs.SetResult(true);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Noget gik galt! Prøv igen.");
+                    Console.ReadKey();
+                }
+            }
+        });
+        return tcs.Task;
+    }
+}
